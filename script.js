555de26e67212d13ca58ab361dd10715fec43067
Olaleye Victor <olaleyevictor541@gmail.com>
2025-04-07 17:07:20 -0700

    new update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // It toggles the "open" class on both the menu and the hamburger icon to trigger CSS styles/animations.
 function toggleMenu(){
     const menu = document.querySelector(".menu-links"); // Get the menu element
-    const icon = document.querySelector(".hamburger-icon"); // Get the hamburger icon
+    // const icon = document.querySelector(".hamburger-icon"); // Get the hamburger icon
     menu.classList.toggle("open"); // Toggle the "open" class to show/hide the menu
-    icon.classList.toggle("open"); // Toggle the "open" class for animation on the icon
+    // icon.classList.toggle("open"); // Toggle the "open" class for animation on the icon
 }
